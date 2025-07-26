e3917d50cae14c56450356e919987efabba6e40b
MaloWinrhy <malo@arkada.fr>
2025-07-26 16:43:13 +0200

    feat: enhance JSON handling with in-place editing and add test files

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use clap::Parser;
+use std::fs;
 use serde_json::Value;
 mod domain;
 mod usecase;
@@ -13,16 +14,24 @@ struct Args {
 
     #[arg(short, long)]
     output: Option<String>,
+
+    #[arg(long)]
+    in_place: bool,
 }
+
 
 fn main() {
     let args = Args::parse();
     let json = infrastructure::file_io::read_json_file(&args.input);
     let cleaned = usecase::clean_json_usecase::CleanJsonUsecase::execute(json);
-    if let Some(output_path) = args.output {
-        infrastructure::file_io::write_json_file(&output_path, &cleaned);
+    let output = serde_json::to_string_pretty(&cleaned).expect("Failed to serialize cleaned JSON");
+    if args.in_place {
+        fs::write(&args.input, &output).expect("Failed to overwrite input file");
+    } else if let Some(output_path) = args.output {
+        fs::write(output_path, &output).expect("Failed to write output");
     } else {
-        println!("{}", serde_json::to_string_pretty(&cleaned).unwrap());
+        println!("{}", output);
     }
+
 }
 
